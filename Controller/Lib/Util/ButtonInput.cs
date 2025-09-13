950f45fdad5348d122dd4267e9f5692fce337bd8
PinkDisorder <git@vividvoid.net>
2025-09-13 16:39:27 +0300

    Depend on the config for code decision

--- Controller/Lib/Util/ButtonInput.cs
+++ Controller/Lib/Util/ButtonInput.cs
@@ -3,12 +3,19 @@ using Controller.Enums;
 
 namespace Controller.Lib.Util;
 
-public class ButtonInput {
-	public readonly int Code;
-	public readonly string Name;
+public class ButtonInput(string name) {
+	
+	public readonly int Code = Core.Config.ControllerType switch {
+		"PS5" => (int)Enum.Parse<DualSense>(name, true),
+		"XBOX" => (int)Enum.Parse<Xbox>(name, true),
+		"SWITCH" => (int)Enum.Parse<Switch>(name, true),
+		_ => -1
+	};
+
+	public readonly string Name = name;
 
 	private const float HeldThreshold = 0.5f;
-	
+
 	private const float DeltaThreshold = HeldThreshold + 0.1f;
 
 	private float DeltaTime { get; set; }
@@ -18,7 +25,7 @@ public class ButtonInput {
 	public bool IsHeld => DeltaTime > HeldThreshold;
 
 	public bool IsActive => DeltaTime > 0;
-	
+
 	public void OnPress(float deltaTime) {
 		// No point in exceeding this.
 		if (DeltaTime >= DeltaThreshold) return;
@@ -26,23 +33,4 @@ public class ButtonInput {
 	}
 
 	public void OnRelease() => DeltaTime = 0;
-
-	public ButtonInput(string name) {
-		Name = name;
-
-		if (name.Contains("DualSense")) {
-			Enum.TryParse(name, out DualSense ds);
-			Code = (int)ds;
-			return;
-		}
-
-		if (name.Contains("Switch")) {
-			Enum.TryParse(name, out Switch sw);
-			Code = (int)sw;
-			return;
-		}
-
-		Enum.TryParse(name, out Xbox xb);
-		Code = (int)xb;
-	}
 }
