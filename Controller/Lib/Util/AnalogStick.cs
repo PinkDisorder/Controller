d5ebf14f1afa9c0fb29a38131ef88546435d7fa6
PinkDisorder <git@vividvoid.net>
2025-09-25 00:57:06 +0300

    Maybe actually store the axes so they're not being shoved in an update loop how about that.

--- Controller/Lib/Util/AnalogStick.cs
+++ Controller/Lib/Util/AnalogStick.cs
@@ -2,27 +2,26 @@ using System;
 
 namespace Controller.Lib.Util;
 
-public class AnalogStick(float x, float y) {
+public class AnalogStick(int xAxis, int yAxis, float x, float y) {
+
 	public float X { get; private set; } = x;
 
 	public float Y { get; private set; } = y;
 
-	public void Update(
-		int xAxis,
-		int yAxis,
-		ReadOnlySpan<float> axes,
-		bool invertY = false
-	) {
+	private const float NoiseThreshold = 0.02f;
+
+	public void Update(ReadOnlySpan<float> axes, bool invertY = false) {
 		if (axes.Length <= Math.Max(xAxis, yAxis)) return;
 
 		float x = Math.Abs(axes[xAxis]) < Core.Config.Deadzone ? 0f : axes[xAxis];
 		float y = Math.Abs(axes[yAxis]) < Core.Config.Deadzone ? 0f : axes[yAxis];
 
-		bool xDidMove = Math.Abs(X - x) > Core.Config.NoiseThreshold;
-		bool yDidMove = Math.Abs(Y - y) > Core.Config.NoiseThreshold;
+		bool xDidMove = Math.Abs(X - x) > NoiseThreshold;
+		bool yDidMove = Math.Abs(Y - y) > NoiseThreshold;
 
 		if (!xDidMove && !yDidMove) return;
 		X = x;
 		Y = invertY ? -y : y;
 	}
+
 }
