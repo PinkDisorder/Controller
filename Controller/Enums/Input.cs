562c8d9162c141aacaeaff4c8c98d4d5284f0a1f
PinkDisorder <git@vividvoid.net>
2025-09-25 01:00:51 +0300

    Move to normalized GLFW enums

--- Controller/Enums/Input.cs
+++ Controller/Enums/Input.cs
@@ -1,99 +1,31 @@
-
 namespace Controller.Enums;
-// These were tested on a PS5 controller, need tester for xbox
-public enum DualSense {
-	X = 0,
-	A = 1,
-	B = 2,
-	Y = 3,
 
-	Lb = 4,
-	Rb = 5,
+public enum GamepadButton {
 
-	Lt = 6,
-	Rt = 7,
+	A = 0
+	, B = 1
+	, X = 2
+	, Y = 3
+	, LeftBumper = 4
+	, RightBumper = 5
+	, Back = 6
+	, Start = 7
+	, Guide = 8
+	, LeftThumb = 9
+	, RightThumb = 10
+	, DPadUp = 11
+	, DPadRight = 12
+	, DPadDown = 13
+	, DPadLeft = 14
 
-	L3 = 10,
-	R3 = 11,
-
-	Back = 8,
-	Start = 9,
-
-	Guide = 12,
-	TouchpadClick = 13,
-	Mute = 14,
-
-	DPadUp = 15,
-	DPadRight = 16,
-	DPadDown = 17,
-	DPadLeft = 18,
-
-	Unknown
 }
+public enum GamepadAxis {
 
-// TODO: Get correct codes
-public enum Xbox {
-	X = 0,
-	A = 1,
-	B = 2,
-	Y = 3,
+	LeftX = 0
+	, LeftY = 1
+	, RightX = 2
+	, RightY = 3
+	, LeftTrigger = 4
+	, RightTrigger = 5
 
-	Lb = 4,
-	Rb = 5,
-
-	Lt = 6,
-	Rt = 7,
-
-	L3 = 10,
-	R3 = 11,
-
-	Back = 8,
-	Start = 9,
-
-	Guide = 12,
-	TouchpadClick = 13,
-	Mute = 14,
-
-	DPadUp = 15,
-	DPadRight = 16,
-	DPadDown = 17,
-	DPadLeft = 18,
-
-	Unknown
-}
-
-public enum Switch {
-	A = 0,
-	B = 1,
-	X = 3,
-	Y = 4,
-
-	Lb = 6,
-	Rb = 7,
-
-	Lt = 8,
-	Rt = 9,
-
-	Back = 10, // minus
-	Start = 11, // plus
-	Guide = 12, // jayu who so kindly reported these said theres an 8bitdo symbol, im guess is the home button
-
-	L3 = 13,
-	R3 = 14,
-
-	DPadUp = 20,
-	DPadRight = 21,
-	DPadDown = 22,
-	DPadLeft = 23,
-	Unknown
-}
-
-public enum Stick {
-	Left,
-	Right
-}
-
-public enum Trigger {
-	Lt,
-	Rt
 }
