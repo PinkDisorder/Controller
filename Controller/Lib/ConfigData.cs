57ada915476ab91234289fb4c417cd51552acd3b
PinkDisorder <git@vividvoid.net>
2025-09-25 00:59:54 +0300

    Add keybinds. No longer need to specify controller type.

--- Controller/Lib/ConfigData.cs
+++ Controller/Lib/ConfigData.cs
@@ -1,35 +1,25 @@
+using System;
+using Controller.Enums;
+
 namespace Controller.Lib;
 
 public class ConfigData {
-	public string ControllerType = "PS5";
 
-	/**
-	 * Treat this like a percentage. 1.00f would be 100%.
-	 * 0.15f or 15% is a good catch-all. Older controllers
-	 * might need larger numbers. Anything over 0.25f is
-	 * most likely overkill.
-	 * Even if your controller is brand new, I suggest
-	 * keeping the deadzone to at least around 0.10f.
-	 */
 	public float Deadzone = 0.15f;
-
-	/**
-	 * This is the percentage of stick movement that
-	 * should be considered noise and ignored.
-	 * It's separate from the deadzone because its meant
-	 * to ignore imperceptible movements, such as
-	 * vibrations caused by simply being held.
-	 * Anything over 0.05f is most likely overkill.
-	 */
-	public float NoiseThreshold = 0.02f;
 
 	// keybinds
 	public string Jump = "A";
-	public string Sprint = "L3";
-	public string Sneak = "R3";
-
 	public string SwitchHands = "B";
-	public string SwitchTool = "X";
+	public string SelectTool = "X";
+	public string Inventory = "Y";
+	public string DropItem = "DPadUp";
+	public string CharacterPanel = "DPadDown";
+	public string LeftClick = "RightTrigger";
+	public string RightClick = "RightBumper";
+	public string Sprint = "LeftThumb";
+	public string Sneak = "RightThumb";
+	public string Menu = "Start";
+	public string Chat = "Guide";
+	public string Map = "Back";
 
-	public string OpenInventory = "Y";
 }
