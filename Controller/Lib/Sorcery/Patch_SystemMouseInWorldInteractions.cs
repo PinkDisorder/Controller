9d3bc227b499a2d8b23844cdbcc2030c1d74f581
PinkDisorder <git@vividvoid.net>
2025-09-25 11:47:51 +0300

    Patch to stop the game from reading mouse inputs outside of guis.

---
+++ Controller/Lib/Sorcery/Patch_SystemMouseInWorldInteractions.cs
@@ -0,0 +1,17 @@
+using HarmonyLib;
+using Vintagestory.Client.NoObf;
+
+namespace Controller.Lib.Sorcery;
+
+[HarmonyPatch(typeof(SystemMouseInWorldInteractions))]
+public static class Patch_SystemMouseInWorldInteractions {
+
+	[HarmonyPrefix]
+	[HarmonyPatch("UpdatePicking")]
+	public static bool UpdatePicking_Prefix(SystemMouseInWorldInteractions __instance, float dt) {
+		var clientMain = (ClientMain)AccessTools.Field(typeof(SystemMouseInWorldInteractions), "game").GetValue(__instance);
+
+		return !clientMain.MouseGrabbed;
+	}
+
+}
