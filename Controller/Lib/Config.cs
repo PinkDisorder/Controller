757c1f8bbcadeac2e98653f6d809399368202da8
PinkDisorder <git@vividvoid.net>
2025-09-27 10:04:01 +0300

    Move Config off to its own class so I can later handle changes

---
+++ Controller/Lib/Config.cs
@@ -0,0 +1,48 @@
+using System;
+using Vintagestory.API.Common;
+
+namespace Controller.Lib;
+
+public class Config {
+
+	public ConfigData Data { get; private set; }
+
+	private readonly ICoreAPI _api;
+	private readonly Mod _mod;
+
+	private void TryWriteConfig(ConfigData data) {
+		try {
+			_api.StoreModConfig(data, $"{_mod.Info.ModID}.json");
+		}
+		catch (Exception e) {
+			_mod.Logger.Error("Config file could not be written");
+			_mod.Logger.Fatal(e);
+		}
+	}
+
+	private ConfigData TryReadConfig() {
+		try {
+			var data = _api.LoadModConfig<ConfigData>($"{_mod.Info.ModID}.json");
+			return data ?? GetDefault();
+		}
+		catch (Exception e) {
+			_mod.Logger.Error("Config file could not be read");
+			_mod.Logger.Error(e);
+
+			return GetDefault();
+		}
+
+		ConfigData GetDefault() {
+			var defaultData = new ConfigData();
+			TryWriteConfig(defaultData);
+			return defaultData;
+		}
+	}
+
+	public Config(ICoreAPI api, Mod mod) {
+		_mod = mod;
+		_api = api;
+		Data = TryReadConfig();
+	}
+
+}
