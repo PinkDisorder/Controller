6e6606d3be520757eae1e8cf56762592ba890871
PinkDisorder <git@vividvoid.net>
2025-10-01 09:01:44 +0300

    Update Config.cs to account for potentially missing or invalid declarations.

--- Controller/Lib/Config.cs
+++ Controller/Lib/Config.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Vintagestory.API.Common;
 
 namespace Controller.Lib;
@@ -7,45 +8,62 @@ namespace Controller.Lib;
 public class Config {
 
 	private readonly ConfigData Data;
+	private readonly ConfigData DefaultData;
+	private string[] KnownKeybinds { get; }
+	private string[] KnownTuning { get; }
+
 	public Dictionary<string, string> Keybinds => Data.Keybinds;
 	public Dictionary<string, float> Tuning => Data.Tuning;
 
-	private readonly ICoreAPI _api;
-	private readonly Mod _mod;
+	private ICoreAPI Capi { get; }
+	private Mod Mod { get; }
 
 	private void TryWriteConfig(ConfigData data) {
 		try {
-			_api.StoreModConfig(data, $"{_mod.Info.ModID}.json");
+			Capi.StoreModConfig(data, $"{Mod.Info.ModID}.json");
+		} catch (Exception e) {
+			Mod.Logger.Error("Config file could not be written");
+			Mod.Logger.Fatal(e);
 		}
-		catch (Exception e) {
-			_mod.Logger.Error("Config file could not be written");
-			_mod.Logger.Fatal(e);
-		}
+	}
+
+	private ConfigData GetDefault() {
+		var defaultData = new ConfigData();
+		TryWriteConfig(defaultData);
+		return defaultData;
 	}
 
 	private ConfigData TryReadConfig() {
 		try {
-			var data = _api.LoadModConfig<ConfigData>($"{_mod.Info.ModID}.json");
-			return data ?? GetDefault();
-		}
-		catch (Exception e) {
-			_mod.Logger.Error("Config file could not be read");
-			_mod.Logger.Error(e);
+			var data = Capi.LoadModConfig<ConfigData>($"{Mod.Info.ModID}.json");
 
+			if (data is null) {
+				return GetDefault();
+			}
+
+			bool satisfy1 = data.Keybinds.Keys.ToArray().SequenceEqual(KnownKeybinds);
+			bool satisfy2 = data.Tuning.Keys.ToArray().SequenceEqual(KnownTuning);
+
+			if (satisfy1 && satisfy2) {
+				return data;
+			}
+
+			Mod.Logger.Error("Config File is corrupt. Attempting to recreate.");
 			return GetDefault();
-		}
-
-		ConfigData GetDefault() {
-			var defaultData = new ConfigData();
-			TryWriteConfig(defaultData);
-			return defaultData;
+		} catch (Exception e) {
+			Mod.Logger.Error("Config file could not be read");
+			Mod.Logger.Error(e);
+			return GetDefault();
 		}
 	}
 
 	public Config(ICoreAPI api, Mod mod) {
-		_mod = mod;
-		_api = api;
-		Data = TryReadConfig();
+		Mod           = mod;
+		Capi          = api;
+		DefaultData   = new ConfigData();
+		KnownKeybinds = DefaultData.Keybinds.Keys.ToArray();
+		KnownTuning   = DefaultData.Tuning.Keys.ToArray();
+		Data          = TryReadConfig();
 	}
 
 }
