a83b1c60298cc00b7f6ac9ac5558cc3b04e49349
PinkDisorder <git@vividvoid.net>
2025-10-04 11:06:42 +0300

    Implement config validation

--- Controller/Lib/Config.cs
+++ Controller/Lib/Config.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Controller.Enums;
 using Vintagestory.API.Common;
 
 namespace Controller.Lib;
 
 public class Config {
+
+	private readonly static string[] ValidButtons = Enum.GetNames(typeof(GamepadButton));
 
 	private readonly ConfigData _data;
 	private string[] KnownKeybinds { get; }
@@ -17,42 +20,62 @@ public class Config {
 	private ICoreAPI Capi { get; }
 	private Mod Mod { get; }
 
-	private void TryWriteConfig(ConfigData data) {
-		try {
-			Capi.StoreModConfig(data, $"{Mod.Info.ModID}.json");
-		} catch (Exception e) {
-			Mod.Logger.Error("Config file could not be written");
-			Mod.Logger.Fatal(e);
-		}
-	}
-
 	private ConfigData GetDefault() {
 		var defaultData = new ConfigData();
 		TryWriteConfig(defaultData);
 		return defaultData;
 	}
 
+	private bool ValidateConfig(ConfigData data) {
+		foreach ((string key, string value) in data.Keybinds) {
+			if (!KnownKeybinds.Contains(key) || !ValidButtons.Contains(value)) {
+				return false;
+			}
+		}
+
+		foreach ((string key, float value) in data.Tuning) {
+			if (!KnownTuning.Contains(key) || value < 0f || value > 1f) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private void TryWriteConfig(ConfigData data) {
+		try {
+			Capi.StoreModConfig(data, $"{Mod.Info.ModID}.json");
+		} catch (Exception e) {
+			Mod.Logger.Error("Config file could not be written.");
+			Mod.Logger.Fatal(e);
+		}
+	}
+
 	private ConfigData TryReadConfig() {
 		try {
 			var data = Capi.LoadModConfig<ConfigData>($"{Mod.Info.ModID}.json");
 
-			if (data is null) {
-				return GetDefault();
-			}
+			if (data is null) return GetDefault();
 
-			bool satisfy1 = data.Keybinds.Keys.ToArray().SequenceEqual(KnownKeybinds);
-			bool satisfy2 = data.Tuning.Keys.ToArray().SequenceEqual(KnownTuning);
+			if (ValidateConfig(data)) return data;
 
-			if (satisfy1 && satisfy2) {
-				return data;
-			}
-
-			Mod.Logger.Error("Config File is corrupt. Attempting to recreate.");
+			Mod.Logger.Error("Config File is corrupt. Replacing with default.");
+			Mod.Logger.Error("Please consult the documentation before making further edits.");
 			return GetDefault();
 		} catch (Exception e) {
-			Mod.Logger.Error("Config file could not be read");
+			Mod.Logger.Error("Config file could not be read or was not found.");
 			Mod.Logger.Error(e);
+			Mod.Logger.Notification("Creating default config file.");
 			return GetDefault();
+		}
+	}
+
+	public void UpdateKeybind(string keybind, string key) {
+		if (KnownKeybinds.Contains(keybind) && ValidButtons.Contains(key)) {
+			_data.Keybinds[keybind] = key;
+			Controls.ReloadKeybinds();
+		} else {
+			Mod.Logger.Error($"Potentially unknown keybind {keybind} or unknown key ${key}");
 		}
 	}
 
